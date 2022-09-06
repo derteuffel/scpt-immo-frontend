a0084f12046537bff0e972acd58ea1353a9b4b5c
derteuffel <derteuffel0@gmail.com>
2022-09-06 17:28:26 +0100

    update port

--- server.js
+++ server.js
@@ -13,6 +13,6 @@ res.sendFile(path.join(__dirname+'/dist/scpt-immo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(4200);
+app.listen(3000);
 
-console.log("[STARTING SERVER] : Listening 4200 ... " + (new Date()));
+console.log("[STARTING SERVER] : Listening 3000 ... " + (new Date()));
