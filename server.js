b88d61d743c6f4b99564502a44b67bde2bd32c95
derteuffel <derteuffel0@gmail.com>
2022-09-06 17:11:52 +0100

    config

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/yesb-inspire'));
+app.use(express.static(__dirname + '/dist/scpt-immo'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/yesb-inspire/index.html'));
+res.sendFile(path.join(__dirname+'/dist/scpt-immo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(4201);
+app.listen(4200);
 
 console.log("[STARTING SERVER] : Listening 4200 ... " + (new Date()));
